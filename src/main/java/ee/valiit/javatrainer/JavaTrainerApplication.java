d7e98132db7b852a6df4786bf9b34245181b6f82
Leo Tamm <leo.tamm@gmail.com>
2020-11-23 15:20:39 +0200

    Initial project commit

---
+++ src/main/java/ee/valiit/javatrainer/JavaTrainerApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.javatrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/javatrainer/JavaTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.javatrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
