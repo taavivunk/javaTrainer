4bb596288443b90b56ab5053f13211a501f003ef
taavivunk <taavi.vunk@gmail.com>
2020-11-24 13:20:45 +0200

    kontrolleri jms põhjad

--- src/main/java/ee/valiit/javatrainer/controller/TrainerController.java
+++ src/main/java/ee/valiit/javatrainer/controller/TrainerController.java
@@ -2,6 +2,9 @@ package ee.valiit.javatrainer.controller;
 
 import ee.valiit.javatrainer.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,9 @@ public class TrainerController {
 @Autowired
     TrainerService trainerService;
 
-
+//    @CrossOrigin
+//    @PostMapping("trainer/newQuestionSet")
+//    public void createQuestion(@RequestBody()){
+//        return ;
+//    }
 }
--- src/main/java/ee/valiit/javatrainer/service/TrainerService.java
+++ src/main/java/ee/valiit/javatrainer/service/TrainerService.java
@@ -4,11 +4,14 @@ package ee.valiit.javatrainer.service;
 import ee.valiit.javatrainer.repository.TrainerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class TrainerService {
 
-@Autowired
+    @Autowired
     private TrainerRepository trainerRepository;
+
 
 }
