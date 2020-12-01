da169bfe718b5b994eb589b4604f80d5280e9e50
taavivunk <taavi.vunk@gmail.com>
2020-12-01 11:55:19 +0200

    01.12. muudatused

---
+++ src/main/java/ee/valiit/javatrainer/controller/ResultBack.java
@@ -0,0 +1,28 @@
+package ee.valiit.javatrainer.controller;
+
+import java.util.List;
+
+public class ResultBack {
+
+    String studentName;
+    List<ResultObjects> resultObject;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public List<ResultObjects> getResultObject() {
+        return resultObject;
+    }
+
+    public void setResultObject(List<ResultObjects> resultObject) {
+        this.resultObject = resultObject;
+    }
+}
+
+
+
