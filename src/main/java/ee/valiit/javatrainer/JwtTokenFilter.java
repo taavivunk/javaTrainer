c4e055de550637f86cb82bb291528411624fa335
taavivunk <taavi.vunk@gmail.com>
2020-12-04 13:35:32 +0200

    jwtToken korras

--- src/main/java/ee/valiit/javatrainer/JwtTokenFilter.java
+++ src/main/java/ee/valiit/javatrainer/JwtTokenFilter.java
@@ -1,17 +1,52 @@
 package ee.valiit.javatrainer;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.GenericFilterBean;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class JwtTokenFilter implements Filter {
+public class JwtTokenFilter extends GenericFilterBean {
+
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String token = resolveToken((HttpServletRequest) request);
+        if (token != null) {
+            Authentication authentication = validateToken(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
     }
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    String resolveToken(HttpServletRequest req) {
+        String headerAttribute = req.getHeader("Authorization");
+        if (headerAttribute == null) {
+            return null;
+        }
+        if (headerAttribute.startsWith("Bearer")) {
+            return headerAttribute.substring(7);
+        }
+        return headerAttribute;
+    }
 
+
+    Authentication validateToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody();
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<GrantedAuthority>();
+        grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+        UserDetails userDetails = new User((String) claims.get("userName"), "", grantedAuthorityList);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     @Override
